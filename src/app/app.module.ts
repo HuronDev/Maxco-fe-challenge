c4f1d8472e5e1e6afa9602723046f1c4bdf2acc2
Huron <charlie.walls97@gmail.com>
2025-09-05 07:55:59 -0500

    feat: create clientes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './modules/clientes/clientes/clientes.component';
+import { ClientesFormComponent } from './modules/clientes/clientes-form/clientes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    ClientesFormComponent
   ],
   imports: [
     BrowserModule,
