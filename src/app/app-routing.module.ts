772fba5c2e2edafbc282170e6e0824f8375373fc
Huron <charlie.walls97@gmail.com>
2025-09-05 08:25:03 -0500

    reafctor: add a path of each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesFormComponent } from './modules/clientes/clientes-form/clientes-form.component';
+import { VendedoresVistaComponent } from './modules/vendedor/vendedores-vista/vendedores-vista.component';
 
 const routes: Routes = [
   
+        {
+          path: 'clientes',
+          component: ClientesFormComponent,
+        },
+        {
+          path: 'vendedores',
+          component: VendedoresVistaComponent,
+        },
+        {
+          path: 'clientes',
+          component: ClientesFormComponent,
+        },
         {
           path: 'clientes',
           component: ClientesFormComponent,
