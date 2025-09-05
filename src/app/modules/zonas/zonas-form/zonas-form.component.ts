7085baa6b9840e99b7433fd5eb9598c2e1196380
Huron <charlie.walls97@gmail.com>
2025-09-05 14:41:55 -0500

    feat: create a new component called zonas form

---
+++ src/app/modules/zonas/zonas-form/zonas-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zonas-form',
+  templateUrl: './zonas-form.component.html',
+  styleUrl: './zonas-form.component.scss'
+})
+export class ZonasFormComponent {
+
+}
--- src/app/modules/zonas/zonas.module.ts
+++ src/app/modules/zonas/zonas.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ZonasVistaComponent } from './zonas-vista/zonas-vista.component';
-import { SharedModule } from '../../Shared/shared.module';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ZonasFormComponent } from './zonas-form/zonas-form.component';
 
 @NgModule({
-  declarations: [ZonasVistaComponent],
+  declarations: [ZonasFormComponent, ZonasVistaComponent],
   imports: [
     CommonModule,
-    SharedModule,
-  ]
+    ReactiveFormsModule,
+    TableModule,
+    ButtonModule,
+    DialogModule,
+    InputTextModule,
+    ToastModule,
+  ],
+    exports: [ZonasVistaComponent,
+      ]
 })
-export class ZonasModule { }
+export class ZonasModule {}
